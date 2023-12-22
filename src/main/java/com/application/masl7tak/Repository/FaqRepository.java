009636d154c7414f689aad37e9879b31dfbf29e3
AhmedQotb <50117886+AhmedQotbAhmed@users.noreply.github.com>
2023-12-22 15:48:29 +0200

    update Business

--- src/main/java/com/application/masl7tak/Repository/FaqRepository.java
+++ src/main/java/com/application/masl7tak/Repository/FaqRepository.java
@@ -10,8 +10,7 @@ import java.util.List;
 
 @Repository
 public interface FaqRepository extends JpaRepository<Faq, Long> {
-    @Query("select f from Faq f where f.answer_ar=null ORDER BY f.id DESC ")
-
+    @Query("SELECT f FROM Faq f WHERE f.answer_ar IS NULL ORDER BY f.id DESC")
     List<Faq> findAllSupport();
 //    (Long id, String name, String email, String question_en, String answer_en, Long user_id, int status)
 @Query(value = "SELECT new com.application.masl7tak.dto.FaqDTO(f.id, f.name, f.email, f.question_en, f.answer_en, f.user_id, f.status ) " +
