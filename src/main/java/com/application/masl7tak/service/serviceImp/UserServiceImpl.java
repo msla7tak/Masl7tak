305242dd5f2e3e895642769531b661b65831bd0c
AhmedQotb <50117886+AhmedQotbAhmed@users.noreply.github.com>
2023-12-25 00:00:11 +0200

    update Business

--- src/main/java/com/application/masl7tak/service/serviceImp/UserServiceImpl.java
+++ src/main/java/com/application/masl7tak/service/serviceImp/UserServiceImpl.java
@@ -130,9 +130,12 @@ public class UserServiceImpl implements UserService {
 
 
                         User inviter = userRepository.findByInvitationCode(requestMap.get("invitation_code"));
+                                if (inviter == null) {
+
+
                         Integer point = replacementRepository.getReferenceById(1L).getPoint_for_invitation();
                         userRepository.updatePoints((inviter.getPoints() + point), inviter.getId());
-
+                                }
                             }
                         }
                     userRepository.updatePoints((replacementRepository.getReferenceById(1L).getPoint_for_registration()), userDTO.getId());
